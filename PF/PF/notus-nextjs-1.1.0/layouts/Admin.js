0a22c4fdf8fd5326aee7302292ffc38eb310fc7c
Nathan Hansen <nathanajhansen@gmail.com>
2022-12-07 15:28:14 -0500

    removed footer and background color

--- PF/PF/notus-nextjs-1.1.0/layouts/Admin.js
+++ PF/PF/notus-nextjs-1.1.0/layouts/Admin.js
@@ -11,15 +11,17 @@ export default function Admin({ children }) {
   return (
     <>
 	  {/* <Sidebar /> */}
-      <div className="relative bg-blueGray-100">
+      <div className="relative ">
         <AdminNavbar />
         {/* Header */}
         <GenericHeader />
         <div className="px-4 md:px-10 mx-auto w-full" style={{marginTop: "-10em"}}>
           {children}
-          <FooterAdmin />
         </div>
       </div>
+	  {/*
+          <FooterAdmin />
+	  */}
     </>
   );
 }
