73c3c2ac6868205f9097211ec9b001c9128ac5ac
Nathan Hansen <nathanajhansen@gmail.com>
2022-12-10 11:14:02 -0500

    fixed profile view logout

--- PF/PF/notus-nextjs-1.1.0/pages/accounts/profile.js
+++ PF/PF/notus-nextjs-1.1.0/pages/accounts/profile.js
@@ -4,6 +4,7 @@ import Navbar from "components/Navbars/AccountNavbar.js";
 import Footer from "components/Footers/Footer.js";
 import authHeader from "services/authHeader";
 import Router from "next/router";
+import logout from "services/logout";
 
 import ClassHistory from "components/ClassHistory";
 
@@ -97,6 +98,7 @@ const Profile = () => {
                         className="bg-blueGray-700 active:bg-blueGray-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150"
                         type="button"
 	  		href="/accounts/logout"
+	  		onClick={logout}
                       >
                         Log Out
                       </a>
