89be92fc7e6a683a7ebb198c58869b3fd758a65e
RealFakeAccount <63186767+RealFakeAccount@users.noreply.github.com>
2022-12-10 18:09:58 -0500

    remove fg

--- PF/run.sh
+++ PF/run.sh
@@ -1,4 +1,4 @@
 BACKEND_DIR="$(pwd)/PB/csc309-tfc-pb"
 FRONTEND_DIR="$(pwd)/PF/notus-nextjs-1.1.0"
 
-python3 $BACKEND_DIR/manage.py runserver & npm run dev --prefix $FRONTEND_DIR && fg
+python3 $BACKEND_DIR/manage.py runserver & npm run dev --prefix $FRONTEND_DIR 
