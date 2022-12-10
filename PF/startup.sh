4f7e9c884615387e4da2d427ab99ea67b2cd4f69
RealFakeAccount <63186767+RealFakeAccount@users.noreply.github.com>
2022-12-10 16:21:35 -0500

    wrongly delete studio.json

--- PF/startup.sh
+++ PF/startup.sh
@@ -22,5 +22,5 @@ $BACKEND_DIR/manage.py createsuperuser \
 --no-input
 
 # populate the database
-python3 $BACKEND_DIR/manage.py loaddata ./studios.json
+python3 $BACKEND_DIR/manage.py loaddata $BACKEND_DIR/startup/management/commands/data/studio.json
 python3 $BACKEND_DIR/manage.py popdb
