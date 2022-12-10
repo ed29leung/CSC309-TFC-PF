a141fbd455618e4339019d771e0fa1a03cbc3ac5
Nathan Hansen <nathanajhansen@gmail.com>
2022-12-10 01:02:09 -0500

    removed subscribe button from account login/modification/create views

--- PF/PF/notus-nextjs-1.1.0/pages/accounts/profile.js
+++ PF/PF/notus-nextjs-1.1.0/pages/accounts/profile.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 
-import Navbar from "components/Navbars/AuthNavbar.js";
+import Navbar from "components/Navbars/AccountNavbar.js";
 import Footer from "components/Footers/Footer.js";
 import authHeader from "services/authHeader";
 import Router from "next/router";
